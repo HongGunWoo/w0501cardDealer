33a7d2f561eb76daf0c116845159da8b2c0e26de
HongGunWoo <foxrain.gg@gmail.com>
2022-10-05 15:36:47 +0900

    스마트앱프로그래밍 5주차 카드딜러

--- app/src/main/java/kr/ac/kumoh/s20181270/w0501carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20181270/w0501carddealer/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.ac.kumoh.s20181270.w0501carddealer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kr.ac.kumoh.s20181270.w0501carddealer.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,6 +12,53 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.card1.setImageResource(R.drawable.c_2_of_hearts)
+//        binding.card1.setImageResource(R.drawable.c_2_of_hearts)
+//        val c = Random.nextInt(52)
+//        val res = resources.getIdentifier(
+//            getCardName(c),
+//            "drawable",
+//            packageName
+//        )
+//
+//        binding.card1.setImageResource(res)
+        binding.btnDeal.setOnClickListener {
+            val c = IntArray(5)
+            val res = IntArray(5)
+
+            for (i in c.indices) {
+                c[i] = Random.nextInt(52)
+
+                res[i] = resources.getIdentifier(
+                    getCardName(c[i]),
+                    "drawable",
+                    packageName
+                )
+            }
+            binding.card1.setImageResource(res[0])
+            binding.card2.setImageResource(res[1])
+            binding.card3.setImageResource(res[2])
+            binding.card4.setImageResource(res[3])
+            binding.card5.setImageResource(res[4])
+        }
+    }
+
+    private fun getCardName(c: Int) : String {
+        val shape = when(c/13){
+            0 -> "spades"
+            1 -> "diamonds"
+            2 -> "hearts"
+            3 -> "clubs"
+            else -> "error"
+        }
+
+        val number = when(c%13){
+            0 -> "ace"
+            in 1..9 -> (c % 13 + 1).toString()
+            10 -> "jack"
+            11 -> "queen"
+            12 -> "king"
+            else -> "error"
+        }
+        return "c_${number}_of_${shape}"
     }
 }
